0315c0bbac5b9480634abea9001347efc50624d7
ZERRAI Sami <sami.zerrai@gmail.com>
2023-03-18 17:34:56 +0100

    add typeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRootAsync(
+    {
+      useFactory: () => ({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'password',
+      database: 'poke_db',
+      entities: [
+          __dirname + '/../**/*.entity{.ts}',
+      ],
+      synchronize: true,
+    })
+  }
+  )],
   controllers: [AppController],
   providers: [AppService],
 })
