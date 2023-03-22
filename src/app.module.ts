30804140e50ae3458576a96665d100b1ab1e8ad7
jeremy.jumpertz@gmail.com <jeremy.jumpertz@gmail.com>
2023-03-22 21:39:32 +0100

    feat: delete test controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,6 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { TypeOrmConfig } from "./config/typeorm.config";
-import { MyEndpointController } from "./controllers/my-endpoint.controller";
-import { AuthController } from "./controllers/auth.controller";
-import { AuthService } from "./services/auth.service";
 import { JwtModule } from "@nestjs/jwt";
 
 @Module({
@@ -16,7 +13,7 @@ import { JwtModule } from "@nestjs/jwt";
       signOptions: { expiresIn: "1h" },
     }),
   ],
-  controllers: [AppController, MyEndpointController, AuthController],
-  providers: [AppService, AuthService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
