c2fae589bc2fa9b64b57ab6d1f26a1b142d9f9e4
Arkeonn <tgeoffron@myges.fr>
2023-04-14 11:41:16 +0200

    Fix: Adding Interceptors + Swagger group and examples
    fix: changing user service

--- src/domains/auth/auth.controller.ts
+++ src/domains/auth/auth.controller.ts
@@ -1,15 +1,26 @@
-import { Body, Controller, HttpException, Inject, Post } from "@nestjs/common";
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  HttpException,
+  Inject,
+  Post,
+  UseInterceptors,
+} from "@nestjs/common";
 import { User } from "../users/users.entity";
 import { AuthService } from "./auth.service";
 import { LoginDto } from "./dto/login.dto";
 import { RegisterDto } from "./dto/register.dto";
+import { ApiTags } from "@nestjs/swagger";
 
+@ApiTags("Auth")
 @Controller()
 export class AuthController {
   @Inject(AuthService)
   private readonly authService: AuthService;
 
   @Post("register")
+  @UseInterceptors(ClassSerializerInterceptor)
   public register(@Body() body: RegisterDto): Promise<User | HttpException> {
     return this.authService.register(body);
   }
