0ba5fd3343a28e5e25dc3f7353e143c0afb8e73b
Adel Sen <adelsenhadjii@gmail.com>
2023-04-23 17:35:14 +0200

    Feat: add logger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
-import { ValidationPipe, VersioningType } from "@nestjs/common";
+import { LogLevel, ValidationPipe, VersioningType } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import InitSwagger from "./config/swagger.config";
 import { AppModule } from "./app.module";
 import { Amin } from "./types/special/main/types";
 
 async function initApp(): Amin {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: [
+      "error",
+      ...(process.env.STAGE === "local" ? ["warn", "debug", "log"] : []),
+    ] as LogLevel[],
+  });
 
   app.enableCors({
     origin: [/localhost:3000/, /\.vercel\.app$/],
@@ -17,6 +22,7 @@ async function initApp(): Amin {
   app.setGlobalPrefix("v1");
 
   InitSwagger(app);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   await app.listen(5000);
